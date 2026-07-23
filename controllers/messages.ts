682204a5fb19629a68f0548e39bce65d7beda4a8
Angel Reyes <ar731684@gmail.com>
2026-07-23 00:58:57 -0600

    method get for chat messages

--- controllers/messages.ts
+++ controllers/messages.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from "express";
+import { pool } from "../database/db";
 
-export default function getMessages (req: Request, res: Response) {
+export async function getMessages (req: Request, res: Response) {
     try {
+
+        const conversation_id = req.params.conversationID;
+
+        const consulta = `SELECT * FROM messages WHERE conversation_id = ${conversation_id}`;
+
+        const data = await pool.query(consulta);
+
+        return res.status(200).json(
+            data.rows
+        )
 
     } catch (error: any) {
         res.status(400).json({
